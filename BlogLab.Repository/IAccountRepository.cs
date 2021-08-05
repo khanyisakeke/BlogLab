e9a0291eda06d21a5da5260236205654b94ebace
Khanyisa <kekekhanyisa@gmail.com>
2021-08-05 16:09:15 +0200

    Models project complete. Repository project created with Account Repository.

---
+++ BlogLab.Repository/IAccountRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BlogLab.models.Account;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlogLab.Repository
+{
+    public interface IAccountRepository
+    {
+
+        public Task<IdentityResult> CreateAsync(ApplicationUserIdentity user,
+            CancellationToken cancellationToken);
+
+        public Task<ApplicationUserIdentity> GetByUsernameAscync(string normalizedUsername,
+            CancellationToken cancellationToken);
+
+    }
+}
---
+++ BlogLab.models/Account/ApplicationUserLogin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogLab.models.Account
+{
+    public class ApplicationUserLogin
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [MinLength(5, ErrorMessage ="Must be at least 5 - 20 characters")]
+        [MaxLength(20, ErrorMessage ="Must be at least 5 - 20 characters")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(10, ErrorMessage = "Must be at least 10 - 20 characters")]
+        [MaxLength(20, ErrorMessage = "Must be at least 10 - 20 characters")]
+        public string Password { get; set; }
+
+    }
+}
